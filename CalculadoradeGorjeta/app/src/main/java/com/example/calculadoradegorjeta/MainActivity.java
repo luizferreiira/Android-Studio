31542334752cebc029efaa254c9ce866a08319bf
luizferreiira <luizferreiraa34@gmail.com>
2022-01-17 00:48:03 -0300

    CalculadoradeGorjeta/

--- CalculadoradeGorjeta/app/src/main/java/com/example/calculadoradegorjeta/MainActivity.java
+++ CalculadoradeGorjeta/app/src/main/java/com/example/calculadoradegorjeta/MainActivity.java
@@ -3,12 +3,81 @@ package com.example.calculadoradegorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textPorcentagem;
+    private TextView textGorjeta;
+    private TextView textTotal;
+    private SeekBar seekBarGorjeta;
+
+    private double porcentagem = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editValor           = findViewById(R.id.editValor);
+        textPorcentagem     = findViewById(R.id.textPorcentagem);
+        textGorjeta         = findViewById(R.id.textGorjeta);
+        textTotal           = findViewById(R.id.textTotal);
+        seekBarGorjeta      = findViewById(R.id.seekBarGorjeta);
+
+        //Adicionar Listener SeekBar
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                porcentagem = progress;
+                textPorcentagem.setText(Math.round( porcentagem ) + " %");
+                calcular();
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void calcular(){
+        String valorRecuperado = editValor.getText().toString();
+
+        if(valorRecuperado == null || valorRecuperado.equals("") ){
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Digite um valor primeiro",
+                    Toast.LENGTH_LONG
+            ).show();
+
+        }else{
+
+            //Converter String para double
+            double valorDigitado = Double.parseDouble(valorRecuperado);
+
+            //Calculo da gorjeta e total
+            double gorjeta = valorDigitado * (porcentagem/100);
+            double total = gorjeta + valorDigitado;
+
+            //exibir a gorjeta e o total
+            textGorjeta.setText( "R$ " + gorjeta );
+            textTotal.setText( "R$ " + total );
+
+        }
     }
 }
